202d121ed8ed5f7d8b4df15b189991d562754d53
gabrielbraga-alt <gabriel.braga@wiliamoliveira.com.br>
2026-07-25 22:08:38 -0300

    Add files via upload

---
+++ painel-cpl/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Painel from './Painel.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Painel />
+  </React.StrictMode>
+);
